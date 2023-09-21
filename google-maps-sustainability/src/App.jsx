8fd11ad27dd0ad1be28e0ca61541c4aeb0cab7c1
the-hugo <101996060+the-hugo@users.noreply.github.com>
2023-09-21 18:36:55 +0200

    ---

--- google-maps-sustainability/src/App.jsx
+++ google-maps-sustainability/src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css';
 import React from 'react';
-import { GoogleMap, LoadScript, Marker, HeatMap } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, HeatmapLayer } from '@react-google-maps/api';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+const gradient = [
+  "rgba(0, 255, 255, 0)",
+  "rgba(0, 255, 255, 1)",
+  "rgba(0, 191, 255, 1)",
+  "rgba(0, 127, 255, 1)",
+  "rgba(0, 63, 255, 1)",
+  "rgba(0, 0, 255, 1)",
+  "rgba(0, 0, 223, 1)",
+  "rgba(0, 0, 191, 1)",
+  "rgba(0, 0, 159, 1)",
+  "rgba(0, 0, 127, 1)",
+  "rgba(63, 0, 91, 1)",
+  "rgba(127, 0, 63, 1)",
+  "rgba(191, 0, 31, 1)",
+  "rgba(255, 0, 0, 1)"
+];
 
 const containerStyle = {
   width: '100vw',
@@ -23,22 +39,45 @@ function App() {
     // Fetch data from the URL
     axios.get('https://lite-air.app/backend/data')
       .then(response => {
-        setData(response.data);
+        const responseData = response.data;
+
+        // Filter and modify the data
+        const filteredData = responseData.filter(item => item.hasOwnProperty('timestamp'));
+        const modifiedData = filteredData.map(item => {
+          // Create a new object with the modified keys
+          const { latitude, longitude, ...rest } = item;
+          return { lat: latitude, lng: longitude, ...rest };
+        });
+
+        // Update the state with the modified data
+        setData(modifiedData);
         setLoading(false);
-        console.log(data)
+
+        // Log the data here
+        console.log(modifiedData);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
         setLoading(false);
       });
   }, []);
+
+  // Render loading indicator while data is being fetched
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  const libraries = ["visualization"];
+
   return (
     <div className="App">
-      <LoadScript
-        googleMapsApiKey="AIzaSyAGZoSeRrwH7zI9rR5OUxTGh--4ePQPItA
-" // Replace with your Google Maps API key
-      >
+      <LoadScript googleMapsApiKey="AIzaSyAGZoSeRrwH7zI9rR5OUxTGh--4ePQPItA" libraries={libraries}>
         <Map center={center} />
+        <HeatmapLayer
+          gradient={gradient}
+          data={data}
+          opacity={1}
+          radius={20}
+        ></HeatmapLayer>
       </LoadScript>
     </div>
   );
