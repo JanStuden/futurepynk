04fe462038adc7ef4da8f184d8319cc2bfd504a5
Jan <j.matthiesen@campus.lmu.de>
2023-09-21 09:54:55 +0200

    add fastet route for comparison

--- controllers/data.js
+++ controllers/data.js
@@ -15,8 +15,12 @@ export const findbestRoute = async (req,res) => {
         const destination = req.query.destination;
         const origin = req.query.origin;
         let polutionCounter =  {"O3": 0.0, "CO2": 0.0, "NO2": 0.0, "NO": 0.0, "CO": 0.0};
-        let totalRoute = {totalPolution: 100000.0, totalBestRoute: null};
-        let co2Route = {totalPolution: 100000.0, totalBestRoute: null};
+        let totalRoute = {type:"totalMinimum", totalPolution: 100000.0, totalBestRoute: null};
+        let co2Route = {type:"CO2Minimum", totalPolution: 100000.0, totalBestRoute: null};
+        let NO2Route = {type:"NO2Minimum", totalPolution: 100000.0, totalBestRoute: null};
+        let NORoute = {type:"NOMinimum", totalPolution: 100000.0, totalBestRoute: null};
+        let CORoute = {type:"COMinimum", totalPolution: 100000.0, totalBestRoute: null};
+        let fastestRoute = {type:"fastest", duration: 100000.0, totalPolution: 100000.0, totalCO2: 0.0, totalNO2: 0.0, totalNO: 0.0, totalCO: 0.0, totalO3: 0.0, totalBestRoute: null};
 
         const query = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=AIzaSyA3dDaBj1LBp_smJQqbICMH76Z5gUGLrtg&alternatives=true`
         const response = await axios.get(query);
@@ -47,9 +51,32 @@ export const findbestRoute = async (req,res) => {
                 co2Route.totalPolution = polutionCounter.CO2;
                 co2Route.totalBestRoute = route;
             }
+            if(polutionCounter.NO2 < NO2Route.totalPolution){
+                NO2Route.totalPolution = polutionCounter.NO2;
+                NO2Route.totalBestRoute = route;
+            }
+            if(polutionCounter.NO < NORoute.totalPolution){
+                NORoute.totalPolution = polutionCounter.NO;
+                NORoute.totalBestRoute = route;
+            }
+            if(polutionCounter.CO < CORoute.totalPolution){
+                CORoute.totalPolution = polutionCounter.CO;
+                CORoute.totalBestRoute = route;
+            }
+            if(fastestRoute.duration > route.legs[0].duration.value){
+                fastestRoute.duration = route.legs[0].duration.value;
+                fastestRoute.totalBestRoute = route;
+                fastestRoute.totalPolution = totalPolution;
+                fastestRoute.totalCO2 = polutionCounter.CO2;
+                fastestRoute.totalNO2 = polutionCounter.NO2;
+                fastestRoute.totalNO = polutionCounter.NO;
+                fastestRoute.totalCO = polutionCounter.CO;
+                fastestRoute.totalO3 = polutionCounter.O3;
+            }
 
         }
-        res.status(200).json(data.routes);
+        let bestRoutes = [fastestRoute, totalRoute, co2Route, NO2Route, NORoute, CORoute]
+        res.status(200).json(bestRoutes);
         console.log(totalRoute.totalPolution);
         console.log(co2Route.totalPolution)
     }
