89ab5f3c8e4ce2fa6d59c56b5328dd369db63475
the-hugo <101996060+the-hugo@users.noreply.github.com>
2023-09-21 10:19:54 +0200

    basic component

--- google-maps-sustainability/src/App.jsx
+++ google-maps-sustainability/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
-
+import "./components/MainScreen.jsx"
+import MainScreenComp from './components/MainScreen.jsx';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <MainScreenComp></MainScreenComp>
       </header>
     </div>
   );
