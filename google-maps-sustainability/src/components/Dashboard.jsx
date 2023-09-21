9d9ba744ad40c454b3e8e19348708b73af9a4f7a
the-hugo <101996060+the-hugo@users.noreply.github.com>
2023-09-21 17:49:12 +0200

    gmaps

--- google-maps-sustainability/src/components/Dashboard.jsx
+++ google-maps-sustainability/src/components/Dashboard.jsx
@@ -1,24 +1,24 @@
 import { useMemo } from "react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
-const Dashboard = function () {
-  useLoadScript({ googleMapsAPIKey: "AIzaSyAGZoSeRrwH7zI9rR5OUxTGh--4ePQPItA" });
+// const Dashboard = function () {
+//   useLoadScript({ googleMapsAPIKey: "AIzaSyAGZoSeRrwH7zI9rR5OUxTGh--4ePQPItA" });
 
-  return (
-    <>
-      <Map />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Map />
+//     </>
+//   );
+// }
 
-function Map() {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+// function Map() {
+//   const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
   
-  return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
-    </GoogleMap>
-  );
-}
+//   return (
+//     <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+//       <Marker position={center} />
+//     </GoogleMap>
+//   );
+// }
 
 export default Dashboard;
