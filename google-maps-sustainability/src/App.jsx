e51de63c33ed86a268391f6b8eb3db0d62c017f1
the-hugo <101996060+the-hugo@users.noreply.github.com>
2023-09-21 17:37:20 +0200

    gmap api

--- google-maps-sustainability/src/App.jsx
+++ google-maps-sustainability/src/App.jsx
@@ -1,13 +1,58 @@
 import './App.css';
-import "./components/MainScreen.jsx"
-import MainScreenComp from './components/MainScreen.jsx';
+import React from 'react';
+import { GoogleMap, LoadScript, Marker, HeatMap } from '@react-google-maps/api';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+
+const center = {
+  lat: 53.5511,
+  lng: 9.9937,
+};
+
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch data from the URL
+    axios.get('https://lite-air.app/backend/data')
+      .then(response => {
+        setData(response.data);
+        setLoading(false);
+        console.log(data)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-      <MainScreenComp></MainScreenComp>
-      </header>
+      <LoadScript
+        googleMapsApiKey="AIzaSyAGZoSeRrwH7zI9rR5OUxTGh--4ePQPItA
+" // Replace with your Google Maps API key
+      >
+        <Map center={center} />
+      </LoadScript>
     </div>
+  );
+}
+
+function Map({ center }) {
+  return (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+    >
+      <Marker position={center} />
+    </GoogleMap>
   );
 }
 
